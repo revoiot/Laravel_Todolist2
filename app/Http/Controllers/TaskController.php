d12d5e9f796dcbcc0cde4b8806677ae9824caa6f
revoiot <revowater@gmail.com>
2020-09-05 14:31:06 +0900

    第7章の途中まで

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Task; // ★ 追加
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -17,12 +18,51 @@ class TaskController extends Controller
         $current_folder = Folder::find($id);
     
         // 選ばれたフォルダに紐づくタスクを取得する
-        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
     
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
             'tasks' => $tasks,
+        ]);
+    }
+
+
+     /**
+     * GET /folders/{id}/tasks/create
+     */
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        $current_folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
+    }
+
+    /**
+     * GET /folders/{id}/tasks/{task_id}/edit
+     */
+
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task = Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
         ]);
     }
 }
